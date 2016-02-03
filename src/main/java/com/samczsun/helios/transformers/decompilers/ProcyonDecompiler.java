171a7f99e979b3f3dd89937b46b699bbe9d4871d
samczsun <samczsun@gmail.com>
2016-02-02 19:42:27 -0500

    And let's not violate copyright

--- src/main/java/com/samczsun/helios/transformers/decompilers/ProcyonDecompiler.java
+++ src/main/java/com/samczsun/helios/transformers/decompilers/ProcyonDecompiler.java
@@ -12,10 +12,16 @@
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
+ *
+ * Copyright (c) 2015 Mike Strobel
+ *
+ *      This source code is subject to terms and conditions of the Apache License, Version 2.0.
+ *      A copy of the license can be found in the License.html file at the root of this distribution.
+ *      By using this source code in any fashion, you are agreeing to be bound by the terms of the
+ *      Apache License, Version 2.0.
+ *
+ *      You must not remove this notice, or any other, from this software.
  */
-
-
-
 
 package com.samczsun.helios.transformers.decompilers;
 
