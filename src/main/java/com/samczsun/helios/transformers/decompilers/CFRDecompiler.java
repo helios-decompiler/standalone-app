e2090e962d0899bf497a6627b8e1b5807d23f83d
samczsun <samczsun@gmail.com>
2016-03-23 20:13:06 -0400

    Rename dupe members

--- src/main/java/com/samczsun/helios/transformers/decompilers/CFRDecompiler.java
+++ src/main/java/com/samczsun/helios/transformers/decompilers/CFRDecompiler.java
@@ -120,7 +120,8 @@ public class CFRDecompiler extends Decompiler {
         RECOVER_TYPE_HINTS("recovertypehints", "Recover Type Hints", true),
         FORCE_RETURNING_IFS("forcereturningifs", "Force Returning Ifs", true),
         FOR_LOOP_AGG_CAPTURE("forloopaggcapture", "For Loop Aggressive Capture", true),
-        RENAME_ILLEGAL_IDENTIFIERS("renameillegalidents", "Rename illegal identifiers", false);
+        RENAME_ILLEGAL_IDENTIFIERS("renameillegalidents", "Rename illegal identifiers", false),
+        RENAME_DUPE_MEMBERS("renamedupmembers", "Rename duplicated member names", false);
 
         private final String name;
         private final String param;
